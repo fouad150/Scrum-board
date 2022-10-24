c28db41e2a1150df1737f43398e24ec68ec086f5
fouad <fouad@gmail.com>
2022-10-24 23:58:58 +0100

    done

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,4 @@
 
-
-/*var id_to_do_count="to_do_count";
-var id_in_progress_count="in_progress_count";
-var id_done_count="done_count";*/ 
-
-
-//var i=0;
-
 function reloadTasks(){
 
     document.getElementById("todo-ul").innerHTML="";
@@ -22,12 +14,7 @@ function reloadTasks(){
 
     var img ;
     var ul;
-    /*var toDoUl='todo-ul';//id
-    var inProgressUl="inprogress-ul";//id
-    var done="done-ul";//id*/
-
-    //i=0;
-
+    
     for (let index = 0; index < tasks.length; index++) {
         if(tasks[index].status=="To Do"){
             to_do_index++;
@@ -70,24 +57,13 @@ function reloadTasks(){
         </li>`;
         document.getElementById(ul).innerHTML+=reloadTask;
         document.getElementById(id_count).innerText=task_index;
-    
-        //i++;
     }
 }
 
 
 var saveButton=document.getElementById("save");
-
-/*var toDoIcon="question.png";
-var inProgressIcon="work-in-progress.png";
-var Done="check.png";*/
-
-//var index;
-
 saveButton.addEventListener("click",function(){
 
-    //index=i;
-    
     var title=document.getElementById("title").value;
     var feature=document.getElementById("feature");
     var bug=document.getElementById("bug");
@@ -101,28 +77,7 @@ saveButton.addEventListener("click",function(){
     var status=document.getElementById("status").value;
     var date=document.getElementById("date").value;
     var description=document.getElementById("description").value;
-    
-    /*if(status=="To Do"){
-        img = toDoIcon;
-        ul=toDoUl;
-        to_do_index++;
-        task_index=to_do_index;
-        id_count=id_to_do_count;
-
-    }else if(status=="In Progress"){
-        img = inProgressIcon;
-        ul=inProgressUl;
-        in_progress_index++;
-        task_index=in_progress_index;
-        id_count=id_in_progress_count;
-    }else{
-        img = Done;
-        ul=done;
-        done_index++;
-        task_index=done_index;
-        id_count=id_done_count;
-    }*/
-
+ 
     object={
         'title'         :   title,
         'type'          :   selectedType,
@@ -135,42 +90,12 @@ saveButton.addEventListener("click",function(){
     
     reloadTasks();
 
-    /*let task = `
-        <li class="list-group-item d-flex" id="task${index}" >
-            <div><img src="./assets/img/${img}" alt=""></div>
-            <div class="w-100">
-                <div class="fs-3 fw-bold">${tasks[index].title}</div>
-                <div class="">
-                    <div class=""># created in ${tasks[index].date}</div>
-                    <div class="">
-                        ${tasks[index].description}
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            <span class="badge rounded-pill text-bg-primary" >${tasks[index].priority}</span>
-                            <span class="badge rounded-pill text-bg-secondary">${tasks[index].type}</span>
-                        </div>
-                        <div>
-                            <button type="button" class="btn btn-info"  data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="edit(${index})">Edit</button>
-                            <button type="button" class="btn btn-danger" onclick="Delete(${index})" >Delete</button>
-                        </div>
-                
-                    </div>
-                </div>
-            </div>
-        </li>`;
-    
-        document.getElementById(ul).innerHTML+=task;
-        document.getElementById(id_count).innerText=task_index;*/
-        
-        //index++;
-
 })
+
 
 function Edit(index){
     document.getElementById("save").style.display = "none";
     document.getElementById("save_changes").style.display = "block";
-
     document.getElementById("title").value=tasks[index].title;
     if (tasks[index].type=="Feature") {
         document.getElementById("feature").checked=true;
@@ -190,8 +115,8 @@ function Edit(index){
     if(x==1){
         return;
     }
+
     var saveChangesBtn=document.getElementById("save_changes");
-   
     saveChangesBtn.addEventListener("click",function(){
         
         if(x==0){
